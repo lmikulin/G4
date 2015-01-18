87aa648df8142607081c5252030182581a614f06
Liana Mikulin <lmikulin@gmail.com>
2015-01-17 23:23:21 -0700

    Update nukeCanvas.js

--- js/nukeCanvas.js
+++ js/nukeCanvas.js
@@ -4,11 +4,13 @@ function draw(){
         var context = canvas.getContext('2d');
 
         //Declear constants
-        var defaultSize = 10;
-        var defaultRange = 100;
+        var DEFAULT_SIZE = 10;
+        var DEFAULT_RANGE = 100;
 
-        var size = (document.getElementById('size').value.length>0)?document.getElementById('size').value:defaultSize;
-        var range = (document.getElementById('range').value.length>0)?document.getElementById('range').value:defaultRange;
+        var size = (document.getElementById('size').value.length>0) ? document.getElementById('size').value : DEFAULT_SIZE;
+        var range = (document.getElementById('range').value.length>0) ? document.getElementById('range').value : DEFAULT_RANGE;
+
+        context.clearRect ( 0 , 0 , canvas.width, canvas.height );
 
         /* Examples
         var x = (document.getElementById('x').value.length>0)?document.getElementById('x').value:'200';
@@ -40,6 +42,4 @@ function draw(){
         context.beginPath();
         context.arc(canvas.width / 2, canvas.height / 2, size, 0, 2*Math.PI);
         context.stroke();
-
-        setTimeout(draw, 100);
 }
